64fdcc5b007c7435ddba77020155b97009cc93cc
vanea hortolomei <hortolomei1989@gmail.com>
2019-07-02 23:48:32 +0300

    add skeletn style

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ let present = 2019;
 
 console.log(`Your age is ${present - year} years`);
 console.log(`hello world`);
+
+const card = document.querySelector('.card');
+card.addEventListener('click', function(){
+	const skeleton = document.querySelector('.card--skeleton');
+	skeleton.classList.add('active');
+})
